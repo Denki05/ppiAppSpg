3c243cce7b99deade09091cce71f719c3c86672c
Denki05 <106417282+Denki05@users.noreply.github.com>
2024-12-31 00:19:01 +0700

    Refactor footer view to streamline attribution formatting

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,6 @@
 <!-- resources/views/layouts/footer.blade.php -->
 <footer class="bg-light text-center text-lg-start mt-auto">
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0);">
-        © {{ date('Y') }} LS FRAGRANCE. <p class="footer-heart">
-        Made with <g-emoji class="g-emoji" alias="heart" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">
-        <img class="emoji" alt="heart" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png"></g-emoji> by <a href="https://github.com/Denki05">Dev</a>
-        </p>
+        © {{ date('Y') }} LS FRAGRANCE. <p class="footer-heart"> Made with <g-emoji class="g-emoji" alias="heart" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png"><img class="emoji" alt="heart" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png"></g-emoji> by <a href="https://github.com/Denki05">Dev</a></p>
     </div>
 </footer>
