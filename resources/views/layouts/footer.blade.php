1e631e0f091ea85d54f9203e380135e2135ff5a9
Denki05 <106417282+Denki05@users.noreply.github.com>
2024-12-31 00:20:57 +0700

    Add CSS styles to ensure footer sticks to the bottom of the page

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,3 +4,16 @@
         © {{ date('Y') }} LS FRAGRANCE. <p class="footer-heart"> Made with <g-emoji class="g-emoji" alias="heart" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png"><img class="emoji" alt="heart" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png"></g-emoji> by <a href="https://github.com/Denki05">Dev</a></p>
     </div>
 </footer>
+
+<style>
+    html, body {
+        height: 100%;
+    }
+    body {
+        display: flex;
+        flex-direction: column;
+    }
+    .mt-auto {
+        margin-top: auto;
+    }
+</style>
